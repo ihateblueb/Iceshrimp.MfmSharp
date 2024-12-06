909442856694af949a03a645171897e2b32740b5
Laura Hausmann <laura@hausmann.dev>
2024-12-06 18:44:03 +0100

    Improve RandomFuzz

--- Iceshrimp.MfmSharp.RandomFuzz/Program.cs
+++ Iceshrimp.MfmSharp.RandomFuzz/Program.cs
@@ -2,55 +2,52 @@
 using System.Security.Cryptography;
 using Iceshrimp.MfmSharp;
 
-if (args.Contains("--random"))
+Directory.CreateDirectory("results");
+var crashes   = Directory.CreateDirectory("results/crashes");
+var slowdowns = Directory.CreateDirectory("results/slowdowns");
+
+var opts    = new ParallelOptions { MaxDegreeOfParallelism = 32 };
+var charset = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\nabcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+const int length  = 100_000;
+const int slowDur = 50;
+
+long id   = 1; //changeme to restart
+var  fail = 0;
+var  slow = 0;
+
+Parallel.ForEach(GetRandomPayloads(), opts, input =>
 {
-	Directory.CreateDirectory("results");
-	var crashes   = Directory.CreateDirectory("results/crashes");
-	var slowdowns = Directory.CreateDirectory("results/slowdowns");
+	id++;
+	if (id < 0) throw new Exception("count underflow!");
 
-	var opts    = new ParallelOptions { MaxDegreeOfParallelism = 32 };
-	var charset = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\nabcdefghijklmnopqrstuvwxyz".ToCharArray();
-
-	const int length  = 100_000;
-	const int slowDur = 50;
-
-	long id   = 1; //changeme to restart
-	var  fail = 0;
-	var  slow = 0;
-
-	Parallel.ForEach(GetRandomPayloads(), opts, input =>
+	try
 	{
-		id++;
-		if (id < 0) throw new Exception("count underflow!");
+		if (id % 1000 == 0)
+			Console.Write($"\rCompleted {id} payloads ({fail} fail, {slow} slow)");
+		var pre = Stopwatch.GetTimestamp();
 
-		try
+		MfmParser.Parse(input);
+
+		var elapsedMs = (int)Stopwatch.GetElapsedTime(pre).TotalMilliseconds;
+		if (elapsedMs > slowDur)
 		{
-			if (id % 1000 == 0)
-				Console.Write($"\rCompleted {id} payloads ({fail} fail, {slow} slow)");
-			var pre = Stopwatch.GetTimestamp();
-
-			MfmParser.Parse(input);
-
-			var elapsedMs = (int)Stopwatch.GetElapsedTime(pre).TotalMilliseconds;
-			if (elapsedMs > slowDur)
-			{
-				slow++;
-				File.WriteAllText($"results/{slowdowns.Name}/{id}.txt", input);
-			}
+			slow++;
+			File.WriteAllText($"results/{slowdowns.Name}/{id}.txt", input);
 		}
-		catch
-		{
-			fail++;
-			File.WriteAllText($"results/{crashes.Name}/{id}.txt", input);
-		}
-	});
-	return;
-
-	IEnumerable<string> GetRandomPayloads()
+	}
+	catch
 	{
-		while (true)
-		{
-			yield return RandomNumberGenerator.GetString(charset, length);
-		}
+		fail++;
+		File.WriteAllText($"results/{crashes.Name}/{id}.txt", input);
+	}
+});
+return;
+
+IEnumerable<string> GetRandomPayloads()
+{
+	while (true)
+	{
+		yield return RandomNumberGenerator.GetString(charset, length);
 	}
 }
